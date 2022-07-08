b13511515d6d044cbc2eee9b37a683412e78d346
MrGorkiy <brysin95@mail.ru>
2022-07-08 11:32:27 +0300

    Update new

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'debug_toolbar',
+    # 'debug_toolbar',
 ]
 
 MIDDLEWARE = [
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -16,9 +15,9 @@ handler404 = "core.views.page_not_found"
 handler500 = "core.views.server_error"
 handler403 = "core.views.permission_denied"
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
-    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns += static(
+#         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+#     )
+#     urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
